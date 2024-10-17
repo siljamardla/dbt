1ab9dbd10b263465848e1222ab491e7d99b38ad5
Silja Märdla <silja.mardla@bolt.eu>
2024-10-17 14:09:47 +0300

    add semantics

---
+++ dbt_project/models/intermediate/metricflow_time_spine.sql
@@ -0,0 +1,18 @@
+with days as (
+
+    {{
+        dbt_utils.date_spine(
+            'day',
+            "to_date('01/01/2012','mm/dd/yyyy')",
+            "to_date('01/01/2030','mm/dd/yyyy')"
+        )
+    }}
+
+),
+
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+
+select * from final
