123602cc983e731d63078bb1deb27f81c20af536
Silja Märdla <silja.mardla@bolt.eu>
2024-10-17 15:33:00 +0300

    optimise by adding entity_ids to facts

--- dbt_project/models/core/fact_order_event.sql
+++ dbt_project/models/core/fact_order_event.sql
@@ -1,1 +1,7 @@
-SELECT * FROM {{ ref("stg_order_event") }}
+SELECT 
+      soe.*
+    , so.customer_id
+FROM        {{ ref("stg_order_event") }} soe
+LEFT JOIN   {{ ref("stg_order") }}       so
+    ON soe.order_id = so.order_id
+    
--- dbt_project/models/core/fact_support_case_event.sql
+++ dbt_project/models/core/fact_support_case_event.sql
@@ -1,1 +1,10 @@
-SELECT * FROM {{ ref("stg_support_case_event") }}
+SELECT
+    ssce.*
+  , ssc.order_id
+  , so.customer_id
+FROM      {{ ref("stg_support_case_event") }} ssce
+LEFT JOIN {{ ref("stg_support_case") }}       ssc
+    ON ssce.support_case_id = ssc.support_case_id
+LEFT JOIN {{ ref("stg_order") }}              so
+    ON ssc.order_id = so.order_id
+    
