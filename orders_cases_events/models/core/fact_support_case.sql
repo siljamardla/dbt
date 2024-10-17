e8b6554858908f005dc5a51e37e14cdb496262fc
Silja Märdla <silja.mardla@bolt.eu>
2024-10-17 15:33:00 +0300

    optimise by adding entity_ids to facts

--- orders_cases_events/models/core/fact_support_case.sql
+++ orders_cases_events/models/core/fact_support_case.sql
@@ -1,1 +1,6 @@
-SELECT * FROM {{ ref("stg_support_case") }}
+SELECT
+    ssc.*
+  , so.customer_id
+FROM      {{ ref("stg_support_case") }} ssc
+LEFT JOIN {{ ref("stg_order") }}        so
+    ON ssc.order_id = so.order_id
